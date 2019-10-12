f73f20d23f36f925952a5da912866094602c7f06
Kaan Karaoglu <kaankaraoglu@outlook.com>
2019-10-12 23:37:06 +0200

    Added bootstrap and made some drafts for dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,62 @@
 @extends('header')
 @section('content')
     <div class="dashboard-page">
-        <h1 class="dashboard-heading">Dashboard</h1>
-        <h4 class="description" style="text-align: center;">You are logged in!</h4>
-        <a href="{{ route('logout-from-spotify') }}" class="spotify-button">Logout</a>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-3 drawer">
+                    <h1 class="heading">Dashboard</h1>
+                    <h4 class="login-status">Status: Logged In &#129304;</h4>
+                    <h2 class="user-info-heading">User Information</h2>
+                    <p>Username: _kaankaraoglu</p>
+                    <p>Profile link:</p>
+                    <p>Playlists: 123</p>
+                    <p>Followers: 123</p>
+                    <br><br><br><br><br><br>
+                    <br><br><br><br><br><br>
+                    <br><br><br><br><br><br>
+                    <a href="{{ route('logout-from-spotify') }}" class="spotify-button">Logout</a>
+                </div>
+                <div class="col-sm-9 content">
+                    <div class="row">
+                        <div class="card col-sm">
+                            <div class="card-data">50</div>
+                            <div class="card-body">
+                                <h5 class="card-title">Average playlist duration in minutes</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                            </div>
+                        </div>
+
+                        <div class="card col-sm">
+                            <div class="card-data">50</div>
+                            <div class="card-body">
+                                <h5 class="card-title">Average playlist duration in minutes</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                            </div>
+                        </div>
+
+                        <div class="card col-sm">
+                            <div class="card-data">50</div>
+                            <div class="card-body">
+                                <h5 class="card-title">Average playlist duration in minutes</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                            </div>
+                        </div>
+
+                        <div class="card col-sm">
+                            <div class="card-data">50</div>
+                            <div class="card-body">
+                                <h5 class="card-title">Average playlist duration in minutes</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/heim.blade.php
+++ resources/views/heim.blade.php
@@ -3,9 +3,12 @@
     <div class="heim-page">
         <img class="logo" alt="Paragon logo" src="{{ asset('assets/paragon-logo.png') }}">
         <h1 class="title">Paragon</h1>
-        <h4 class="description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </h4>
+        <p class="description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
         <a href="{{ route('redirect-to-spotify') }}" class="spotify-button">Login with Spotify</a>
     </div>
 @endsection
