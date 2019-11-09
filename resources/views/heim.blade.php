4d0c7b10dad016c6bf29b0ea5b7f8f3ffccdce4d
Kaan Karaoglu <kaankaraoglu@outlook.com>
2019-11-09 08:56:14 +0100

    Removed personal website from Paragon

--- resources/views/heim.blade.php
+++ resources/views/heim.blade.php
@@ -3,19 +3,13 @@
     <div class="heim-page">
         <div class="container-fluid">
             <div class="row">
-                <div class="col kaan-col">
-                    {{--<img class="kaan-avatar" alt="Kaan Karaoglu" src="{{ asset('assets/kaan-avatar.png') }}">
-                    <h1 class="title">Kaan Karaoglu</h1>
-                    <p class="description">Personal website</p>--}}
-                    <kaan-karaoglu class="kaan-karaoglu"></kaan-karaoglu>
-                </div>
                 <div class="col paragon-col">
                     <img class="paragon-logo" alt="paragon-logo" src="{{ asset('assets/paragon-logo.png') }}">
                     <p class="title">Paragon</p>
                     <p class="description">
-                        Paragon started off as a side-project to learn Spotify API, Vue.js and Laravel.
-                        It shows average audio features for each of the user's playlists such as BPM, danceability, instrumentalness, energy etc.
-                        accompanied by overall stats.
+                        Paragon is a tool you can use to display average audio features for your playlists, overall stats.
+                        You can also generate playlists based on audio features such as BPM, danceability, instrumentalness, energy and more.
+                        Developed using Spotify API, Laravel and Vue.
                     </p>
                     <a href="{{ route('redirect-to-spotify') }}" class="spotify-button">Login with Spotify</a>
                 </div>
