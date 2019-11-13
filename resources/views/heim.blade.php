59be5857b4609cf40bc81a272ae309d6f9875b0b
Kaan Karaoglu <kaankaraoglu@outlook.com>
2019-11-13 22:51:07 +0100

    Home page styling and description change

--- resources/views/heim.blade.php
+++ resources/views/heim.blade.php
@@ -7,9 +7,9 @@
                     <img class="opus-1-logo" alt="opus-1-logo" src="{{ asset('assets/opus-1-logo.png') }}">
                     <p class="title">Opus 1</p>
                     <p class="description">
-                        Opus 1 is a tool that displays detailed information such as average audio features of your playlists and user statistics.
-                        Opus 1 also generates playlists based on audio features such as BPM, danceability, instrumentalness, energy and more.
-                        Developed using Spotify API, Laravel and Vue. Enjoy!
+                        Opus 1 is a tool that displays interesting information about Spotify playlists, such as average audio features and statistics.
+                        It also generates playlists based on audio features such as BPM, danceability, instrumentalness, energy et cetera.
+                        Powered by Spotify API, Laravel and Vue. Enjoy!
                     </p>
                     <a href="{{ route('redirect-to-spotify') }}" class="spotify-button">Login with Spotify</a>
                 </div>
