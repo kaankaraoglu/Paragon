56794c36454679668fdbc1d6b0fc918eb761f0f1
Kaan Karaoglu <kaankaraoglu@outlook.com>
2019-11-25 14:39:17 +0100

    Removed redundant DashboardTopBar component

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,19 @@
             :fav-tracks="{{ json_encode($favTracks) }}">
         </drawer>
         <div class="col-10 content">
-            <dashboard-top-bar></dashboard-top-bar>
+            <div class="row dashboard-top-bar">
+                <ul class="nav nav-pills" id="pills-tab" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link rounded-pill" id="pills-public-playlists-tab" data-toggle="pill" href="#pills-public-playlists" role="tab" aria-controls="pills-public-playlists" aria-selected="true">Public Playlists</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link rounded-pill" id="pills-private-playlists-tab" data-toggle="pill" href="#pills-private-playlists" role="tab" aria-controls="pills-private-playlists" aria-selected="false">Private Playlists</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link rounded-pill active" id="pills-generate-tab" data-toggle="pill" href="#pills-generate" role="tab" aria-controls="pills-generate" aria-selected="false">Generate</a>
+                    </li>
+                </ul>
+            </div>
             <div class="tab-content playlists-container" id="pills-tabContent">
                 <div class="tab-pane fade show active" id="pills-public-playlists" role="tabpanel" aria-labelledby="pills-public-playlists-tab">
                     <div class="card-columns">
